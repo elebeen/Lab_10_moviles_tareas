804b1574ec80c99bcf50720019339743516bb896
hermes curo <hermescurov5@gmail.com>
2025-05-23 11:22:59 -0500

    Fixing routes for supplier

--- app/src/main/java/com/example/lab_10_moviles_tareas/service/ShopApiService.kt
+++ app/src/main/java/com/example/lab_10_moviles_tareas/service/ShopApiService.kt
@@ -15,54 +15,54 @@ import retrofit2.http.Path
 interface ShopApiService {
 
     // Rutas para los productos
-    @GET("products")
+    @GET("api/products")
     suspend fun getProducts(): ArrayList<Product>
 
-    @GET("products/{id}")
+    @GET("api/products/{id}")
     suspend fun getProduct(@Path("id") id: Int): Response<Product>
 
     @Headers("Content-Type: application/json")
-    @POST("products")
+    @POST("api/products")
     suspend fun createProduct(@Body product: Product): Response<Product>
 
-    @PUT("products/{id}")
+    @PUT("api/products/{id}")
     suspend fun updateProduct(@Path("id") id: Int, @Body product: Product): Response<Product>
 
-    @DELETE("products/{id}")
+    @DELETE("api/products/{id}")
     suspend fun deleteProduct(@Path("id") id: Int): Response<Product>
 
     // Rutas para las categorías
-    @GET("categories")
+    @GET("api/categories")
     suspend fun getCategories(): List<Category>
 
-    @GET("categories/{id}")
+    @GET("api/categories/{id}")
     suspend fun getCategory(@Path("id") id: Int): Response<Category>
 
     @Headers("Content-Type: application/json")
-    @POST("categories")
+    @POST("api/categories")
     suspend fun createCategory(@Body category: Category): Response<Category>
 
-    @PUT("categories/{id}")
+    @PUT("api/categories/{id}")
     suspend fun updateCategory(@Path("id") id: Int, @Body category: Category): Response<Category>
 
-    @DELETE("categories/{id}")
+    @DELETE("api/categories/{id}")
     suspend fun deleteCategory(@Path("id") id: Int): Response<Category>
 
     // Rutas para los proveedores
-    @GET("suppliers")
+    @GET("api/supliers")
     suspend fun getSuppliers(): List<Supplier>
 
-    @GET("suppliers/{id}")
+    @GET("api/supliers/{id}")
     suspend fun getSupplier(@Path("id") id: Int): Response<Supplier>
 
     @Headers("Content-Type: application/json")
-    @POST("suppliers")
+    @POST("api/supliers")
     suspend fun createSupplier(@Body supplier: Supplier): Response<Supplier>
 
-    @PUT("suppliers/{id}")
+    @PUT("api/supliers/{id}")
     suspend fun updateSupplier(@Path("id") id: Int, @Body supplier: Supplier): Response<Supplier>
 
-    @DELETE("suppliers/{id}")
+    @DELETE("api/supliers/{id}")
     suspend fun deleteSupplier(@Path("id") id: Int): Response<Supplier>
 
 }
--- app/src/main/java/com/example/lab_10_moviles_tareas/views/supplier/previews/SupplierPreviews.kt
+++ app/src/main/java/com/example/lab_10_moviles_tareas/views/supplier/previews/SupplierPreviews.kt
@@ -8,58 +8,58 @@ import com.example.lab_10_moviles_tareas.viewModel.fake.FakeSupplierViewModel
 import com.example.lab_10_moviles_tareas.views.supplier.ListSuppliers
 import kotlinx.coroutines.flow.MutableStateFlow
 
-@Preview(showBackground = true, name = "Lista normal")
-@Composable
-fun ListSuppliersPreview_Normal() {
-    Lab_10_moviles_tareasTheme {
-        ListSuppliers(
-            navController = rememberNavController(),
-            viewModel = FakeSupplierViewModel()
-        )
-    }
-}
-
-@Preview(showBackground = true, name = "Lista vacía")
-@Composable
-fun ListSuppliersPreview_Empty() {
-    Lab_10_moviles_tareasTheme {
-        val emptyViewModel = FakeSupplierViewModel().apply {
-            (suppliers as MutableStateFlow).value = emptyList()
-        }
-
-        ListSuppliers(
-            navController = rememberNavController(),
-            viewModel = emptyViewModel
-        )
-    }
-}
-
-@Preview(showBackground = true, name = "Cargando")
-@Composable
-fun ListSuppliersPreview_Loading() {
-    Lab_10_moviles_tareasTheme {
-        val loadingViewModel = FakeSupplierViewModel().apply {
-            (isLoading as MutableStateFlow).value = true
-        }
-
-        ListSuppliers(
-            navController = rememberNavController(),
-            viewModel = loadingViewModel
-        )
-    }
-}
-
-@Preview(showBackground = true, name = "Con error")
-@Composable
-fun ListSuppliersPreview_Error() {
-    Lab_10_moviles_tareasTheme {
-        val errorViewModel = FakeSupplierViewModel().apply {
-            (errorMessage as MutableStateFlow).value = "Error de conexión con el servidor"
-        }
-
-        ListSuppliers(
-            navController = rememberNavController(),
-            viewModel = errorViewModel
-        )
-    }
-}
+//@Preview(showBackground = true, name = "Lista normal")
+//@Composable
+//fun ListSuppliersPreview_Normal() {
+//    Lab_10_moviles_tareasTheme {
+//        ListSuppliers(
+//            navController = rememberNavController(),
+//            viewModel = FakeSupplierViewModel()
+//        )
+//    }
+//}
+//
+//@Preview(showBackground = true, name = "Lista vacía")
+//@Composable
+//fun ListSuppliersPreview_Empty() {
+//    Lab_10_moviles_tareasTheme {
+//        val emptyViewModel = FakeSupplierViewModel().apply {
+//            (suppliers as MutableStateFlow).value = emptyList()
+//        }
+//
+//        ListSuppliers(
+//            navController = rememberNavController(),
+//            viewModel = emptyViewModel
+//        )
+//    }
+//}
+//
+//@Preview(showBackground = true, name = "Cargando")
+//@Composable
+//fun ListSuppliersPreview_Loading() {
+//    Lab_10_moviles_tareasTheme {
+//        val loadingViewModel = FakeSupplierViewModel().apply {
+//            (isLoading as MutableStateFlow).value = true
+//        }
+//
+//        ListSuppliers(
+//            navController = rememberNavController(),
+//            viewModel = loadingViewModel
+//        )
+//    }
+//}
+//
+//@Preview(showBackground = true, name = "Con error")
+//@Composable
+//fun ListSuppliersPreview_Error() {
+//    Lab_10_moviles_tareasTheme {
+//        val errorViewModel = FakeSupplierViewModel().apply {
+//            (errorMessage as MutableStateFlow).value = "Error de conexión con el servidor"
+//        }
+//
+//        ListSuppliers(
+//            navController = rememberNavController(),
+//            viewModel = errorViewModel
+//        )
+//    }
+//}
